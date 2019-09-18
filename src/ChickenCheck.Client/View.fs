59b936de6060132b426e9c9c97683879f4825ee7
Viktor Andersson <v.andersson@me.com>
2019-09-18 13:04:35 +0200

    use fulma

--- src/ChickenCheck.Client/View.fs
+++ src/ChickenCheck.Client/View.fs
@@ -5,46 +5,17 @@ open Fable.Core
 open Fable.Core.JsInterop
 open Fable.React
 open Fable.React.Props
-module Mui = Fable.MaterialUI.Core
 open Fable.MaterialUI.MaterialDesignIcons
 open Fable.MaterialUI.Icons
-open Fable.MaterialUI.Props
-open Fable.MaterialUI.Themes
-
 open ChickenCheck.Client
 open Messages
 open ChickenCheck.Client.Pages
 open Elmish.React.Common
 open ChickenCheck.Domain.Helpers
 open Elmish
+open Fulma
 
-let private styles (theme : ITheme) : IStyles list =
-    [
-        Styles.Toolbar [
-            FlexWrap "wrap"
-        ]
-        Styles.Custom("toolbarTitle", [
-            FlexGrow 1
-        ])
-        Styles.Custom("appBar", [
-            BorderBottom(sprintf "1px solid %s" theme.palette.divider |> Mui.toObj) 
-        ])
-        Styles.Custom("link", [
-            MarginLeft theme.spacing.unit
-            MarginRight (theme.spacing.unit * 2)
-        ])
-        Styles.Custom("bottomNavigation", [
-            Width "100%"
-            CSSProp.Position PositionOptions.Fixed
-            Bottom 0
-        ])
-        Styles.Content [
-            MarginTop theme.spacing.unit
-            MarginBottom theme.spacing.unit
-        ]
-    ]
-
-let private view' (classes: Mui.IClasses) model dispatch =
+let view model dispatch =
 
     let pageHtml (page : Page) =
         match page with
@@ -56,37 +27,16 @@ let private view' (classes: Mui.IClasses) model dispatch =
         | None -> false, ""
         | Some session -> true, session.Name.Value
 
+    let navbar =
+        Navbar.navbar [ Navbar.Color IsInfo ]
+            [ Navbar.Brand.div [ ]
+                [ Navbar.Item.a [ Navbar.Item.Props [ Href "#" ] ]
+                    [ img [ Style [ Width "2.5em" ] // Force svg display
+                            Src "Icons/android-chrome-192x192.png" ] ]  
+                  Navbar.Item.a [ Navbar.Item.Props [ Href "#" ] ]
+                    [ str "Mina hönor" ] ] ]
+
     div [] 
-        [ yield Mui.cssBaseline []
-          if isLoggedIn then
-              yield Mui.appBar 
-                  [ AppBarProp.Position AppBarPosition.Static
-                    Class !!classes?appBar ] 
-                  [ Mui.toolbar 
-                      [ Class !!classes?toolbar ] 
-                      [ Mui.typography 
-                          [ Variant TypographyVariant.H6
-                            Class !!classes?toolbarTitle ] 
-                          [ str "Mina hönor" ]
-                        str loggedInUsername ] ] 
-          yield div [ Class !!classes?content ] [ pageHtml model.CurrentPage ] ] 
-
-
-// Boilerplate below to support JSS with Elmish
-
-type private IProps =
-    abstract member model: Model with get, set
-    abstract member dispatch: (Msg -> unit) with get, set
-    inherit Mui.IClassesProps
-
-type private Component(p) =
-    inherit PureStatelessComponent<IProps>(p)
-    let viewFun (p: IProps) = view' p.classes p.model p.dispatch
-    let viewWithStyles = Mui.withStyles (StyleType.Func styles) [] viewFun
-    override this.render() = ReactElementType.create !!viewWithStyles this.props []
-
-let view (model: Model) (dispatch: Msg -> unit) : ReactElement =
-    let props = jsOptions<IProps>(fun p ->
-        p.model <- model
-        p.dispatch <- dispatch)
-    ofType<Component,_,_> props []
+        [ if isLoggedIn then
+              yield navbar 
+          yield div [ ] [ pageHtml model.CurrentPage ] ] 
